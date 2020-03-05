f833760363ac4933df01b901425bbb8d86a58f00
shashank <shashanksharma580S@gmail.com>
2020-03-05 09:43:38 +0530

    demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, ViewChild, ElementRef } from '@angular/core';
+import * as FusionCharts from 'fusioncharts';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {dataa } from './gdata';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,136 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'demoFusion';
+  dataSource: any;
+  type: string;
+  width: string;
+  height: string;
+  fusion;
+  chart;
+  foods = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+  @ViewChild('graph') someInput: ElementRef;
+  constructor(private http: HttpClient) {
+    this.type = 'timeseries';
+    this.width = '100%';
+    this.height = '400';
+    // This is the dataSource of the chart
+    this.dataSource = {
+      data: null,
+      chart: {
+        timeSpread: {
+          unit: 'day',
+          multiplier: 4
+        },
+        multiCanvas: false,
+        showLegend: 1,
+      },
+      caption: {
+        text: 'Flow Rate vs Time'
+      },
+      series: 'Type',
+      yAxis: [
+        {
+          plot: [
+            {
+            value: 'Daily Visitors',
+            type: 'Line',
+            connectnulldata: true,
+            style: {
+              'plot.null': {
+              'stroke-dasharray': 'none',
+                stroke: '#FF0000'
+              }
+            }
+          }
+          ],
+          title: 'dssasad'
+        }
+      ],
+      xaxis: {
+        initialInterval: {
+            from: "26/02/20 15:00:00",
+            to: "26/02/20 23:59:59"
+        }
+    }
+    };
+    this.fetchData();
+  }
+
+  // In this method we will create our DataStore and using that we will create a custom DataTable which takes two
+  // parameters, one is data another is schema.
+  fetchData() {
+    this.http.get(`http://localhost:9999/rest/spectrum/graph/813/0`, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      })
+    }).subscribe((val: any) => {
+      console.log('done');
+      const data =  val;
+      const schema = [    {
+        'name': 'Time',
+        'type': 'date',
+        'format': '%d/%m/%y %H:%M:%S'
+    }, {
+        'name': 'Type',
+        'type': 'string'
+        },
+        {
+        'name': 'Flow Rate(mL/min)',
+        'type': 'number'
+        }];
+      // console.log(res[1]);
+      // First we are creating a DataStore
+      const fusionDataStore = new FusionCharts.DataStore();
+      console.log(fusionDataStore);
+      // After that we are creating a DataTable by passing our data and schema as arguments
+      const fusionTable = fusionDataStore.createDataTable(data, schema);
+      // Afet that we simply mutated our timeseries datasource by attaching the above
+      // DataTable into its data property.
+      console.log(this.dataSource);
+      this.dataSource.data = fusionTable;
+    });
+  }
+
+  initialized($event) {
+    this.chart = $event.chart; // saving chart instance
+
+    this.http.get(`http://localhost:9999/rest/spectrum/graph/hist/813`, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      })
+    }).subscribe((val: any) => {
+      let i = 0;
+      const time = new Date(1582711218000);
+      const incrementor = setInterval(() => {
+        if (this.chart !== undefined) {
+          time.setSeconds(time.getSeconds() + 10);
+          // console.log(typeof(time.toLocaleString()));
+          const feed = val[i][23].toString();
+          const perm = val[i][41].toString();
+          const ret = val[i][5].toString();
+
+          let formatedTime = time.toLocaleString();
+          formatedTime = formatedTime.replace(",", "");
+          formatedTime = formatedTime.replace("/2020", "/20");
+          const a  = [formatedTime, 'Feed' , feed];
+          console.log(a);
+          this.chart.feedData([
+            [formatedTime, 'Feed', feed],
+            [formatedTime, 'Permeate', perm ],
+            [formatedTime, 'Retentate', ret ]
+          ]);
+          i++;
+          // console.log(this.chart);
+        }
+        // console.log(this.chart);
+
+      }, 1000);
+    });
 }
+
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FusionChartsModule } from 'angular-fusioncharts';
+import * as FusionCharts from 'fusioncharts';
+import * as charts from 'fusioncharts/fusioncharts.charts';
+import * as FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+import * as chartsp from 'fusioncharts/fusioncharts.timeseries';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
+FusionChartsModule.fcRoot(FusionCharts, charts, FusionTheme, chartsp);
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatSelectModule,
+    MatFormFieldModule,
+  HttpClientModule,
+    AppRoutingModule,
+    FusionChartsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
